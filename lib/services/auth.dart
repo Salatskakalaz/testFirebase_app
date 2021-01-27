0d6cddd90b8ca1c0df589bbae4468eaeedde299f
Salatskakalaz <Salatskakalaz@gmail.com>
2021-01-27 16:35:21 +0500

    the abstrac AuhtBase class

--- lib/services/auth.dart
+++ lib/services/auth.dart
@@ -7,7 +7,15 @@ class User {
   final String uid;
 }
 
-class Auth {
+abstract class AuthBase {
+  Future<User> currentUser();
+
+  Future<User> signInAnonymously();
+
+  Future<void> signOut();
+}
+
+class Auth implements AuthBase {
   final _firebaseAuth = FirebaseAuth.instance;
 
   User __userFromFirebase(FirebaseUser user) {
@@ -17,16 +25,19 @@ class Auth {
     return User(uid: user.uid);
   }
 
+  @required
   Future<User> currentUser() async {
     final user = await _firebaseAuth.currentUser();
     return __userFromFirebase(user);
   }
 
+  @required
   Future<User> signInAnonymously() async {
     final authResult = await _firebaseAuth.signInAnonymously();
     return __userFromFirebase(authResult.user);
   }
 
+  @required
   Future<void> signOut() async {
     await _firebaseAuth.signOut();
   }
